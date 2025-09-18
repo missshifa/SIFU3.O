139eed8efdad02f242a59b4a96deebe67bd7ad59
SHIFAT <nilaafrin733@gmail.com>
2025-09-18 23:32:50 +0600

    Update botinfo.js

--- SUJON/commands/botinfo.js
+++ SUJON/commands/botinfo.js
@@ -25,8 +25,8 @@ module.exports.run = async function({ api, event }) {
   const time = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【HH:mm:ss】");
 
   const images = [
-    "https://i.imgur.com/Weh4Q1r.jpeg",
-    "https://i.imgur.com/PT1L0Tw.jpeg"
+    "https://i.imgur.com/DYNNSbX.jpeg",
+    "https://i.imgur.com/DYNNSbX.jpeg"
   ];
 
   const imageLink = images[Math.floor(Math.random() * images.length)];
@@ -39,14 +39,14 @@ module.exports.run = async function({ api, event }) {
         {
           body:
             "╭──────•◈•───────╮\n" +
-            "  —͟͟͞͞𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_☢️  \n\n" +
+            "  ✨𝐇𝐈𝐍𝐀𝐓𝐀 𝐖𝐎𝐑𝐋𝐃✨ \n\n" +
             `☄️𝘽𝙊𝙏𝙉𝘼𝙈𝙀☄️ »» ${global.config.BOTNAME}\n` +
             `🌸𝙋𝙍𝙀𝙁𝙄𝙓🌸  »» ${global.config.PREFIX} ««\n\n` +
             "🥳𝙐𝙋𝙏𝙄𝙈𝙀🥳\n\n" +
             `𝑫𝑨𝑻𝑬 𝑨𝑵𝑫 𝑻𝑰𝑴𝑬 \n${time}\n\n` +
             `⚡𝘽𝙊𝙏 𝙄𝙎 𝙍𝙐𝙉𝙉𝙄𝙉𝙂⚡ \n🕛 ${hours}:${minutes}:${seconds} 🕧\n\n` +
-            `𝐁𝐎𝐓 𝐅𝐎𝐑𝐊 :- https://github.com/cyber-ullash/CYBER-BOT-COMMUNITY\n\n` +
-            `𝗜𝘀𝗹𝗮𝗺𝗶𝗰 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁 | sujon-boss\n` +
+            `—͟͟͞͞sʜɪғꫝ֟፝ؖ۬ᴛ ✿ ᴀᴅᴍɪɴ\n\n` +
+            `𝗜 ✨𝐇𝐈𝐍𝐀𝐓𝐀 𝐖𝐎𝐑𝐋𝐃✨\n` +
             "╰──────•◈•───────╯",
           attachment: fs.createReadStream(filePath)
         },
