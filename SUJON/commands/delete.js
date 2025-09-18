7f8017f8b06094896537080707485054eeb62a19
SHIFAT <nilaafrin733@gmail.com>
2025-09-19 00:25:39 +0600

    Update delete.js

--- SUJON/commands/delete.js
+++ SUJON/commands/delete.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	version: "1.0.1",
 	hasPermssion: 0,
 	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
-	description: "Delete 🖱️",
+	description: "Delete ",
 	commandCategory: "Image",
 	usages: "rank",
 	cooldowns: 5,
