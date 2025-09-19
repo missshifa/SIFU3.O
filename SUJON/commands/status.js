d3e38078fafec647799030e22675072c549710d3
SHIFAT <nilaafrin733@gmail.com>
2025-09-19 06:15:20 +0600

    Update status.js

--- SUJON/commands/status.js
+++ SUJON/commands/status.js
@@ -19,7 +19,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
- var hi = ["--SUJON-BOSS--"];
+ var hi = ["--✨ 𝐇 𝐈 𝐍 𝐀 𝐓 𝐀 ✨--"];
  var know = hi[Math.floor(Math.random() * hi.length)];
  var link = [
 
