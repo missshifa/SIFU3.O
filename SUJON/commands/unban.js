80fc7e2f22e9074042facc3f73ee2563a07c8a1a
SHIFAT <nilaafrin733@gmail.com>
2025-09-19 08:51:38 +0600

    Update unban.js

--- SUJON/commands/unban.js
+++ SUJON/commands/unban.js
@@ -16,7 +16,7 @@ module.exports.run = async ({ event, api, Users, Threads, args }) => {
  const { commands } = global.client;
  const command = commands.get(("unban").toLowerCase());
  const credit = command.config.credits;
- var mangG = "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️";
+ var mangG = "✨𝐇𝐈𝐍𝐀𝐓𝐀 𝐖𝐎𝐑𝐋𝐃✨";
  if(credit != mangG) return api.sendMessage(`Sai credit!`, event.threadID, event.messageID);
  
  const threadSetting = global.data.threadData.get(parseInt(event.threadID)) || {};
